ebad2c3e88d45b2787437b578f6ba60165eb0d3e
Jivko Karov <jikito2007@gmail.com>
2025-05-14 11:35:44 +0300

    zad 1 completed

---
+++ zad 1/Graph.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad_1
+{
+    internal class Graph
+    {
+        private List<int>[] adjacencyList;
+
+        public Graph(int numberOfVertices)
+        {
+            adjacencyList = new List<int>[numberOfVertices];
+
+            for (int i = 0; i < numberOfVertices; i++)
+            {
+                adjacencyList[i] = new List<int>();
+            }
+        }
+
+        public void AddEdge(int vertex1, int vertex2)
+        {
+            adjacencyList[vertex1].Add(vertex2);
+            adjacencyList[vertex2].Add(vertex1); // За неориентиран граф
+        }
+
+        public void RemoveEdge(int vertex1, int vertex2)
+        {
+            adjacencyList[vertex1].Remove(vertex2);
+            adjacencyList[vertex2].Remove(vertex1); // За неориентиран граф
+        }
+
+        public void PrintGraph()
+        {
+            for (int i = 0; i < adjacencyList.Length; i++)
+            {
+                Console.Write($"Връх {i}: ");
+
+                foreach (int neighbor in adjacencyList[i])
+                {
+                    Console.Write($"{neighbor} ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
--- zad 1/Program.cs
+++ zad 1/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Graph graph = new Graph(5); // Създаване на граф с 5 върха (0 до 4)
+
+            graph.AddEdge(0, 1);
+            graph.AddEdge(0, 4);
+            graph.AddEdge(1, 2);
+            graph.AddEdge(1, 3);
+            graph.AddEdge(1, 4);
+            graph.AddEdge(2, 3);
+            graph.AddEdge(3, 4);
+
+            Console.WriteLine("Графът след добавяне на ребра:");
+            graph.PrintGraph();
+
+            graph.RemoveEdge(1, 4);
+            Console.WriteLine("\nГрафът след премахване на реброто между 1 и 4:");
+            graph.PrintGraph();
         }
     }
 }
